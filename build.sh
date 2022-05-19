4bc70faa46c9b6c2cc724c72bc182a0dc30a36ef
fylder <fylder@163.com>
2022-05-19 17:07:15 +0800

    build more arch_abi

--- build.sh
+++ build.sh
@@ -1,17 +1,36 @@
-rm -rf build/CMakeFiles/ahh.dir
-rm build/cmake_install.cmake
-rm build/Makefile
-#rm -rf outputs
 
+function build_so
+{
+  # 移除缓存CMakeCache,让cmake配置生效
+  rm CMakeCache.txt
+
+  cmake -DCMAKE_ANDROID_ARCH_ABI=$AHH_ABI ../
+  make
+  echo "build $AHH_ABI so finish\n"
+}
+
+rm -rf outputs
 mkdir build
 cd build
 
-cmake ../
-make
+# armeabi-v7a
+AHH_ABI=armeabi-v7a
+build_so
+
+# arm64-v8a
+AHH_ABI=arm64-v8a
+build_so
+
+# x86
+AHH_ABI=x86
+build_so
+
+# x86_64
+AHH_ABI=x86_64
+build_so
+
 
 echo "build finish\n"
-
-rm CMakeCache.txt
 
 cd ../outputs
 if [ -f "jinLibs/armeabi-v7a/libahh.so" ]; then
